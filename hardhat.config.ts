fe5631e363831e9e9ceb31a7d6b2b72a2af1d30c
mj52951 <mj52951@fer.hr>
2023-07-10 13:40:26 +0200

    Updated package dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -81,7 +81,7 @@ const config: HardhatUserConfig = {
 
       },
       sepolia: {
-        url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+        url: `https://sepolia.infura.io/v3/${process.env.ALCHEMY_API_KEY}`,
         accounts: [`${process.env.PRIVATE_KEY}`],
       }, 
 
